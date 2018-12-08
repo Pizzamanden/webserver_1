dafa9f85801bc7f2b16fa6df30e9626a6c70622f
Peter Winther <Peter@DESKTOP-U1V5KEO MINGW64 /c/xampp/htdocs$ git fetchPeter@DESKTOP-U1V5KEO MINGW64 /c/xampp/htdocs (master)$ gitusage: git [--version] [--help] [-C path] [-c name=value]           [--exec-path[=path]] [--html-path] [--man-path] [--info-path]           [-p | --paginate | -P | --no-pager] [--no-replace-objects] [--bare]           [--git-dir=path] [--work-tree=path] [--namespace=name]           command [args]These are common Git commands used in various situations:start a working area (see also: git help tutorial)   clone      Clone a repository into a new directory   init       Create an empty Git repository or reinitialize an existing onework on the current change (see also: git help everyday)   add        Add file contents to the index   mv         Move or rename a file, a directory, or a symlink   reset      Reset current HEAD to the specified state   rm         Remove files from the working tree and from the indexexamine the history and state (see also: git help revisions)   bisect     Use binary search to find the commit that introduced a bug   grep       Print lines matching a pattern   log        Show commit logs   show       Show various types of objects   status     Show the working tree statusgrow, mark and tweak your common history   branch     List, create, or delete branches   checkout   Switch branches or restore working tree files   commit     Record changes to the repository   diff       Show changes between commits, commit and working tree, etc   merge      Join two or more development histories together   rebase     Reapply commits on top of another base tip   tag        Create, list, delete or verify a tag object signed with GPGcollaborate (see also: git help workflows)   fetch      Download objects and refs from another repository   pull       Fetch from and integrate with another repository or a local branch   push       Update remote refs along with associated objects'git help -a' and 'git help -g' list available subcommands and someconcept guides. See 'git help command' or 'git help concept'to read about a specific subcommand or concept.Peter@DESKTOP-U1V5KEO MINGW64 /c/xampp/htdocs (master)$ git fetchPeter@DESKTOP-U1V5KEO MINGW64 /c/xampp/htdocs (master)$ git mergefatal: No remote for the current branch.Peter@DESKTOP-U1V5KEO MINGW64 /c/xampp/htdocs (master)$ git pull https://github.com/Pizzamanden/webserver_1.gitremote: Enumerating objects: 2060, done.remote: Counting objects: 100% (2060/2060), done.remote: Compressing objects: 100% (1723/1723), done.remote: Total 2060 (delta 306), reused 2035 (delta 281), pack-reused 0Receiving objects: 100% (2060/2060), 10.38 MiB | 7.78 MiB/s, done.Resolving deltas: 100% (306/306), done.From https://github.com/Pizzamanden/webserver_1 * branch            HEAD       - FETCH_HEADerror: The following untracked working tree files would be overwritten by merge:        apple-touch-icon.png        clientsideopgave/css/style.css        clientsideopgave/sass/_reset.scss        clientsideopgave/sass/_values.scss        clientsideopgave/sass/style.scss        css/main.css        css/reset.css        favicon.ico        include/.htaccess        include/classes/listDir.php        include/code/config.php        index.php        opgaver/flexbox/css/style.css        opgaver/flexbox/images/awesome-logo.svg        opgaver/flexbox/images/five.svg        opgaver/flexbox/images/four.svg        opgaver/flexbox/images/one.svg        opgaver/flexbox/images/social-icons.svg        opgaver/flexbox/images/three.svg        opgaver/flexbox/images/two.svg        opgaver/flexbox/index.html        opgaver/jquery/external/jquery/jquery.js        opgaver/jquery/images/ui-bg_diagonals-thick_15_5b0b6f_40x40.png        opgaver/jquery/images/ui-bg_diamond_30_9015d7_10x8.png        opgaver/jquery/images/ui-bg_diamond_30_a32d00_10x8.png        opgaver/jquery/images/ui-bg_dots-small_20_333333_2x2.png        opgaver/jquery/images/ui-bg_gloss-wave_20_111111_500x100.png        opgaver/jquery/images/ui-bg_highlight-soft_40_292929_1x100.png        opgaver/jquery/images/ui-bg_highlight-soft_40_6b008f_1x100.png        opgaver/jquery/images/ui-icons_43008f_256x240.png        opgaver/jquery/images/ui-icons_db9cfc_256x240.png        opgaver/jquery/images/ui-icons_ffffff_256x240.png        opgaver/jquery/index.html        opgaver/jquery/jqui/jquery-ui.css        opgaver/jquery/jqui/jquery-ui.js        opgaver/jquery/jqui/jquery-ui.min.css        opgaver/jquery/jqui/jquery-ui.min.js        opgaver/jquery/jqui/jquery-ui.structure.css        opgaver/jquery/jqui/jquery-ui.structure.min.css        opgaver/jquery/jqui/jquery-ui.theme.css        opgaver/jquery/jqui/jquery-ui.theme.min.css        opgaver/jquery/jqui/package.json        opgaver/prototypeJS/class.html        opgaver/prototypeJS/constructor.html        opgaver/prototypeJS/dinmor.html        opgaver/prototypeJS/literal.html        opgaver/prototypeJS/object_create.html        opgaver/prototypeJS/performance.html        opgaver/prototypeJS/prototype.html        opgaver/prototypeJS/this.html        opgaver/sasstest/.sass-cache/230bb9986d292d3baea9ddf74da613e79efa7acf/_reset.scssc        opgaver/sasstest/.sass-cache/230bb9986d292d3baea9ddf74da613e79efa7acf/_values.scssc        opgaver/sasstest/.sass-cache/230bb9986d292d3baea9ddf74da613e79efa7acf/style.scssc        opgaver/sasstest/css/style.css        opgaver/sasstest/sass/_reset.scss        opgaver/sasstest/sass/_values.scss        opgaver/sasstest/sass/style.scss        programmeringsopgave/css/style.css        programmeringsopgave/index.html        programmeringsopgave/js/script.js        programmeringsopgave/js/script.min.js        programmeringsopgave/sass/_reset.scss        programmeringsopgave/sass/_values.scss        programmeringsopgave/sass/style.scss        skabelon/css/style.css        skabelon/index.html        wordpress/.htaccess        wordpress/index.php        wordpress/license.txt        wordpress/readme.html        wordpress/wp-activate.php        wordpress/wp-admin/about.php        wordpress/wp-admin/admin-ajax.php        wordpress/wp-admin/admin-footer.php        wordpress/wp-admin/admin-functions.php        wordpress/wp-admin/admin-header.php        wordpress/wp-admin/admin-post.php        wordpress/wp-admin/admin.php        wordpress/wp-admin/async-upload.php        wordpress/wp-admin/comment.php        wordpress/wp-admin/credits.php        wordpress/wp-admin/css/about-rtl.css        wordpress/wp-admin/css/about-rtl.min.css        wordpress/wp-admin/css/about.css        wordpress/wp-admin/css/about.min.css        wordpress/wp-admin/css/admin-menu-rtl.css        wordpress/wp-admin/css/admin-menu-rtl.min.css        wordpress/wp-admin/css/admin-menu.css        wordpress/wp-admin/css/admin-menu.min.css        wordpress/wp-admin/css/code-editor-rtl.css        wordpress/wp-admin/css/code-editor-rtl.min.css        wordpress/wp-admin/css/code-editor.css        wordpress/wp-admin/css/code-editor.min.css        wordpress/wp-admin/css/color-picker-rtl.css        wordpress/wp-admin/css/color-picker-rtl.min.css        wordpress/wp-admin/css/color-picker.css        wordpress/wp-admin/css/color-picker.min.css        wordpress/wp-admin/css/colors/_admin.scss        wordpress/wp-admin/css/colors/_mixins.scss        wordpress/wp-admin/css/colors/_variables.scss        wordpress/wp-admin/css/colors/blue/colors-rtl.css        wordpress/wp-admin/css/colors/blue/colors-rtl.min.css        wordpress/wp-admin/css/colors/blue/colors.css        wordpress/wp-admin/cAbortingPeter@DESKTOP-U1V5KEO MINGW64 /c/xampp/htdocs (master)$ git pull https://github.com/Pizzamanden/webserver_1.gitFrom https://github.com/Pizzamanden/webserver_1 * branch            HEAD       - FETCH_HEADChecking out files: 100% (1651/1651), done.Peter@DESKTOP-U1V5KEO MINGW64 /c/xampp/htdocs (master)$ git remote -v origin ^CPeter@DESKTOP-U1V5KEO MINGW64 /c/xampp/htdocs (master)$ git remote -v origin https://github.com/Pizzamanden/webserver_1.giterror: Unknown subcommand: originusage: git remote [-v | --verbose]   or: git remote add [-t branch] [-m master] [-f] [--tags | --no-tags] [--mirror=fetch|push] name url   or: git remote rename old new   or: git remote remove name   or: git remote set-head name (-a | --auto | -d | --delete | branch)   or: git remote [-v | --verbose] show [-n] name   or: git remote prune [-n | --dry-run] name   or: git remote [-v | --verbose] update [-p | --prune] [(group | remote)...]   or: git remote set-branches [--add] name branch...   or: git remote get-url [--push] [--all] name   or: git remote set-url [--push] name newurl [oldurl]   or: git remote set-url --add name newurl   or: git remote set-url --delete name url    -v, --verbose         be verbose; must be placed before a subcommandPeter@DESKTOP-U1V5KEO MINGW64 /c/xampp/htdocs (master)$ git remote origin https://github.com/Pizzamanden/webserver_1.giterror: Unknown subcommand: originusage: git remote [-v | --verbose]   or: git remote add [-t branch] [-m master] [-f] [--tags | --no-tags] [--mirror=fetch|push] name url   or: git remote rename old new   or: git remote remove name   or: git remote set-head name (-a | --auto | -d | --delete | branch)   or: git remote [-v | --verbose] show [-n] name   or: git remote prune [-n | --dry-run] name   or: git remote [-v | --verbose] update [-p | --prune] [(group | remote)...]   or: git remote set-branches [--add] name branch...   or: git remote get-url [--push] [--all] name   or: git remote set-url [--push] name newurl [oldurl]   or: git remote set-url --add name newurl   or: git remote set-url --delete name url    -v, --verbose         be verbose; must be placed before a subcommandPeter@DESKTOP-U1V5KEO MINGW64 /c/xampp/htdocs (master)$ git remote add origin https://github.com/Pizzamanden/webserver_1.gitPeter@DESKTOP-U1V5KEO MINGW64 /c/xampp/htdocs (master)$ git config --global user.email>
2018-12-08 18:30:59 +0100

    completed resize script for minHeight

--- clientsideopgave/js/script.js
+++ clientsideopgave/js/script.js
@@ -1,9 +1,16 @@
 (function(){
+    var header = $("#html_header");
+    var footer = $("#html_footer");
+    var main = $("#html_content");
+    var viewPort = $(window);
     
-
-
-
-
-
+    function setContentHeight(){
+        main.css("minHeight", viewPort.height() - footer.height() - header.height());
+    }
+    setContentHeight();
+    viewPort.resize(function() {
+        setContentHeight();
+        console.log("fired resize");
+    });
 
 })();
